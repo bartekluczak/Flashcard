ce21083cecb7ddbedd0fc80799ccc661fec1aeb7
Bartosz Kołtoniak <bartosz.koltoniak@outlook.com>
2021-06-21 02:41:16 +0200

    MenuHelper for inserting menus into templates. Group controller

---
+++ src/Helpers/MenuHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Helpers;
+
+class MenuHelper
+{
+    private $menuItems = array(
+        array('name' => 'Learn', 'order' => 0, 'url' => '/learn', 'active' => false),
+        array('name' => 'Groups', 'order' => 1, 'url' => '/groups', 'active' => false),
+        array('name' => 'Statistics', 'order' => 2, 'url' => '/statistics', 'active' => false),
+        array('name' => 'Logout', 'order' => 3, 'url' => '/logout', 'active' => false)
+    ); 
+
+    public function getMenu($name) {
+        $menu = $this->menuItems;
+        for ($i = 0; $i < count($menu); $i++){
+            if($menu[$i]['name'] == $name){
+                $menu[$i]['active'] = true;
+            }
+        }
+        return $menu;
+    }
+}
