c60ca30eea731ceb44d196e17029647a89015fac
BartekLuczak <b.luczak0@gmail.com>
2021-06-21 22:26:11 +0200

    Add GroupController

---
+++ src/Controller/_____GroupController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Helpers\MenuHelper;
+
+class _____GroupController extends AbstractController
+{
+    #[Route('/groups', name: 'groups')]
+    public function index(): Response
+    {   
+        $menu = new MenuHelper();
+
+        return $this->render('group/index.html.twig', [
+            'controller_name' => 'GroupController',
+            'menu' => $menu->getMenu('Groups')
+        ]);
+    }
+}
