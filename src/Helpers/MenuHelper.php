1b0da78830bf937f9491e1d47e9f6935c4b039d1
Bartosz Kołtoniak <bartosz.koltoniak@outlook.com>
2021-06-24 00:29:59 +0200

    polish menu labels

--- src/Helpers/MenuHelper.php
+++ src/Helpers/MenuHelper.php
@@ -5,10 +5,10 @@ namespace App\Helpers;
 class MenuHelper
 {
     private $menuItems = array(
-        array('name' => 'Learn', 'order' => 0, 'url' => '/learn', 'active' => false),
-        array('name' => 'Groups', 'order' => 1, 'url' => '/group', 'active' => false),
-        array('name' => 'Statistics', 'order' => 2, 'url' => '/statistics', 'active' => false),
-        array('name' => 'Logout', 'order' => 3, 'url' => '/logout', 'active' => false)
+        array('name' => 'Nauka', 'order' => 0, 'url' => '/learn', 'active' => false),
+        array('name' => 'Grupy', 'order' => 1, 'url' => '/group', 'active' => false),
+        array('name' => 'Statystyki', 'order' => 2, 'url' => '/statistics', 'active' => false),
+        array('name' => 'Wyloguj', 'order' => 3, 'url' => '/logout', 'active' => false)
     ); 
 
     public function getMenu($name) {
