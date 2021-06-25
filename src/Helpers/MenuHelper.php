eee2d6aeb1d2a7de6afcee7627a4c83d98669fdc
Bartosz Kołtoniak <bartosz.koltoniak@outlook.com>
2021-06-25 19:22:13 +0200

    fix link to session view

--- src/Helpers/MenuHelper.php
+++ src/Helpers/MenuHelper.php
@@ -5,7 +5,7 @@ namespace App\Helpers;
 class MenuHelper
 {
     private $menuItems = array(
-        array('name' => 'Nauka', 'order' => 0, 'url' => '/learn', 'active' => false),
+        array('name' => 'Nauka', 'order' => 0, 'url' => '/session', 'active' => false),
         array('name' => 'Grupy', 'order' => 1, 'url' => '/group', 'active' => false),
         array('name' => 'Statystyki', 'order' => 2, 'url' => '/statistics', 'active' => false),
         array('name' => 'Wyloguj', 'order' => 3, 'url' => '/logout', 'active' => false)
