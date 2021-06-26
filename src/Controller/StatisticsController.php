ddbc3ceb137ab5e981165a3538f85863a590ce67
Bartosz Kołtoniak <bartosz.koltoniak@outlook.com>
2021-06-26 22:38:41 +0200

    get statistics from all sessions to statistics view

--- src/Controller/StatisticsController.php
+++ src/Controller/StatisticsController.php
@@ -2,21 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\GroupRepository;
+use App\Repository\SessionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+
 
 class StatisticsController extends AbstractController
 {
     #[Route('/statistics', name: 'statistics')]
-    public function index(StatisticsRepository $statsRepo): Response
+    public function index(ManagerRegistry $ManagerRegistry): Response
     {
-        $statistics-> $statsRepo(findAll);
+        $sessionRepository = new SessionRepository($ManagerRegistry);
+        $allSessionStatistics = $sessionRepository->getAllStatistics();
 
-        $catCorr = [];
-        $catIncorr = [];
+
         return $this->render('statistics/index.html.twig', [
             'controller_name' => 'StatisticsController',
+            'allSessionStatistics' => $allSessionStatistics
         ]);
     }
 }
